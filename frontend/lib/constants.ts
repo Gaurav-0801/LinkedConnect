c566ea825faeddc2c918777125abc6ce93670374
Gaurav-0801 <gaurav.pant2021@vitbhopal.ac.in>
2025-08-03 15:48:40 +0530

    Fix prisma generate and catch-all route

--- frontend/lib/constants.ts
+++ frontend/lib/constants.ts
@@ -1,2 +1,2 @@
 // lib/constants.ts
-export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:4000";
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "https://linkedconnect.onrender.com";
